e2b6042f91a6f3939dc8de49d13fad38f293ba9b
hi1004 <kipo09@naver.com>
2023-01-17 05:20:09 +0900

    add Store class in core file

--- src/core/core.js
+++ src/core/core.js
@@ -46,3 +46,32 @@ export function createRouter(routes) {
     routeRender(routes);
   };
 }
+
+//// Store ////
+export class Store {
+  constructor(state) {
+    this.state = {}; // 상태(데이터)
+    this.observers = {};
+    for (const key in state) {
+      // 각 상태에 대한 변경 감시(Setter) 설정!
+      Object.defineProperty(this.state, key, {
+        // Getter
+        get: () => state[key],
+        // Setter
+        set: val => {
+          state[key] = val;
+          // 호출할 콜백이 있는 경우!
+          if (Array.isArray(this.observers[key])) {
+            this.observers[key].forEach(observer => observer(val));
+          }
+        },
+      });
+    }
+  }
+  // 상태 변경 구독
+  subscribe(key, cb) {
+    Array.isArray(this.observers[key]) // 이미 등록된 콜백이 있는지 확인!
+      ? this.observers[key].push(cb) // 있으면 새로운 콜백 밀어넣기!
+      : (this.observers[key] = [cb]); // 없으면 콜백 배열로 할당!
+  }
+}
