fd4adc5a2cc01658a24e3af975e08aa28fa75dbb
hi1004 <kipo09@naver.com>
2023-01-17 05:27:31 +0900

    add id app

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { Component } from './core/core';
 export default class App extends Component {
   render() {
+    this.el.id = 'app';
     const routerView = document.createElement('router-view');
     this.el.append(routerView);
   }
