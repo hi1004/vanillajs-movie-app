da637820e2cb9b9e551e5608609bb2fb8300f31b
hi1004 <kipo09@naver.com>
2023-01-17 05:53:05 +0900

    append search component

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,10 +1,13 @@
 import { Component } from '../core/core';
 import Headline from '../components/Headline';
+import Search from '../components/Search';
 
 export default class Home extends Component {
   render() {
     const headline = new Headline().el;
+    const search = new Search().el;
+
     this.el.classList.add('container');
-    this.el.append(headline);
+    this.el.append(headline, search);
   }
 }
