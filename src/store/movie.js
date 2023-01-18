3385817a6c6d828260182719e8fd90357c26e461
hi1004 <kipo09@naver.com>
2023-01-18 20:07:51 +0900

    update

--- src/store/movie.js
+++ src/store/movie.js
@@ -3,13 +3,22 @@ import { Store } from '../core/core';
 const store = new Store({
   searchText: '',
   page: 1,
+  pageMax: 1,
   movies: [],
+  loading: false,
 });
 export default store;
 export const searchMovies = async page => {
+  store.state.loading = true;
+  store.state.page = page;
+  if (page === 1) {
+    store.state.movies = [];
+  }
   const res = await fetch(
     `https://www.omdbapi.com/?apikey=7035c60c&s=${store.state.searchText}&page=${page}`
   );
-  const json = await res.json();
-  console.log(json, res);
+  const { Search, totalResults } = await res.json();
+  store.state.movies = [...store.state.movies, ...Search];
+  store.state.pageMax = Math.ceil(Number(totalResults) / 10);
+  store.state.loading = false;
 };
