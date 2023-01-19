f10adaed26f872f228033ab6c20a73a23988538e
hi1004 <kipo09@naver.com>
2023-01-20 00:15:38 +0900

    add isValid movie menu hide

--- src/components/TheHeader.js
+++ src/components/TheHeader.js
@@ -9,19 +9,30 @@ export default class TheHeader extends Component {
           {
             name: 'Search',
             href: '#/',
+            isValid: true,
           },
           {
             name: 'Movie',
             href: '#/movie',
+            isValid: false,
           },
           {
             name: 'About',
             href: '#/about',
+            isValid: true,
           },
         ],
         logoURL:
           'https://github.com/hi1004/United-IT-Promotion/blob/master/src/assets/executives/executives5.gif?raw=true',
       },
+    });
+    window.addEventListener('popstate', () => {
+      const href = this.state.menus[1].href.split('?')[0];
+      const hash = location.hash.split('?')[0];
+      const isValid = href === hash;
+      if (isValid) this.state.menus[1].isValid = true;
+
+      this.render();
     });
   }
   render() {
@@ -30,16 +41,21 @@ export default class TheHeader extends Component {
       <nav>
         <ul>
           ${this.state.menus
+            .filter(menu => menu.isValid)
             .map(menu => {
+              const href = menu.href.split('?')[0];
+              const hash = location.hash.split('?')[0];
+              const isActive = href === hash;
               return /* html */ `
               <li>
-                <a href="${menu.href}">
+                <a href="${menu.href}" class="${isActive ? 'active' : ''}">
                   ${menu.name}
                 </a>
               </li>
             `;
             })
             .join('')}
+            
         </ul>
       </nav>
       <a href="#/about" class="user">
