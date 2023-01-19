19c723318c42b09f5a36dc591652af4e08e065c7
hi1004 <kipo09@naver.com>
2023-01-19 20:58:29 +0900

    add TheHeader App.js

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
+import TheHeader from './components/TheHeader';
 import { Component } from './core/core';
 export default class App extends Component {
   render() {
     this.el.id = 'app';
     const routerView = document.createElement('router-view');
-    this.el.append(routerView);
+    const theHeader = new TheHeader().el;
+    this.el.append(theHeader, routerView);
   }
 }
