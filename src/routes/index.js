dc1b7afbf2f8ad8e589cef21a86b229d3bf93113
hi1004 <kipo09@naver.com>
2023-01-20 20:54:07 +0900

    add about, notfound router

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,8 +1,12 @@
 import { createRouter } from '../core/core';
 import Home from './Home';
 import Movie from './Movie';
+import About from './About';
+import NotFound from './NotFound';
 
 export default createRouter([
   { path: '#/', component: Home },
   { path: '#/movie', component: Movie },
+  { path: '#/about', component: About },
+  { path: '.*', component: NotFound },
 ]);
