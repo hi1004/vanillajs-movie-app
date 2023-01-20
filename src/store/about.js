f8648cfaad4c158379803327ac4e556712aa084b
hi1004 <kipo09@naver.com>
2023-01-20 20:51:18 +0900

    create about store

---
+++ src/store/about.js
@@ -0,0 +1,11 @@
+import { Store } from '../core/core';
+
+export default new Store({
+  photo:
+    'https://github.com/hi1004/United-IT-Promotion/blob/master/src/assets/executives/executives5.gif?raw=true',
+  name: 'HI1004 / OhByeongHeon',
+  email: 'byorusia@gmail.com',
+  blog: 'https://velog.io/@kipo09',
+  github: 'https://github.com/hi1004',
+  repository: 'https://github.com/hi1004/vanillajs-movie-app',
+});
