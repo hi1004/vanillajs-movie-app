61036298776028fdd9dc2181ff87ef833506f760
hi1004 <kipo09@naver.com>
2023-01-20 20:52:34 +0900

    append theFooter component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import TheHeader from './components/TheHeader';
+import TheFooter from './components/TheFooter';
 import { Component } from './core/core';
 export default class App extends Component {
   render() {
     this.el.id = 'app';
     const routerView = document.createElement('router-view');
     const theHeader = new TheHeader().el;
-    this.el.append(theHeader, routerView);
+    const theFooter = new TheFooter().el;
+    this.el.append(theHeader, routerView, theFooter);
   }
 }
