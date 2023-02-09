89ece52ff77bcd8c0632b47c48b76e81a92e362f
hi1004 <kipo09@naver.com>
2023-02-09 20:40:37 +0900

    update

--- src/App.ts
+++ src/App.ts
@@ -1,6 +1,7 @@
 import TheHeader from './components/TheHeader';
 import TheFooter from './components/TheFooter';
 import { Component } from './core/core';
+
 export default class App extends Component {
   render() {
     this.el.id = 'app';
