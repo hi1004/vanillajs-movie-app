5e6079bc1f1cc2de09909e0637f633953448c020
hi1004 <kipo09@naver.com>
2023-02-12 23:33:46 +0900

    remove test

--- src/components/TheHeader.ts
+++ src/components/TheHeader.ts
@@ -28,10 +28,6 @@ export default class TheHeader extends Component {
             name: 'About',
             href: '#/about',
           },
-          {
-            name: 'Test',
-            href: '#/',
-          },
         ],
       },
     });
